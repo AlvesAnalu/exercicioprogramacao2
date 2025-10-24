cac53c7e890d22bf531177c7f1a6e425ba43a55a
AlvesAnalu <marsilioanalu@gmail.com>
2025-10-24 13:36:49 -0300

    ex2

--- funcoes.py
+++ funcoes.py
@@ -3,3 +3,8 @@ def define_posicoes(linha, coluna, orientacao, tamanho):
         return [[linha, coluna + i] for i in range(tamanho)]
     elif orientacao == 'vertical':
         return [[linha + i, coluna] for i in range(tamanho)]
+    
+def preenche_frota(frota,nome_navio,linha_navio,coluna_navio,orientacao,tamanho):
+    posic=define_posicoes
+    for nome_navio,posic in frota.items():
+        frota[nome_navio]=posic
