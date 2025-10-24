db9c7f850068fa9b7a10a011521e4112e2a8a03e
AlvesAnalu <marsilioanalu@gmail.com>
2025-10-24 19:52:00 -0300

    ex7

--- programa.py
+++ programa.py
@@ -1,18 +1,25 @@
-frota = {"porta-aviões": [],
+from funcoes import posicao_valida
+from funcoes import preenche_frota
+
+frota = {
+    "porta-avioes": [],
     "navio-tanque": [],
     "contratorpedeiro": [],
-    "submarino": []}
+    "submarino": []
+}
 
-navios_info = {"porta-aviões": {"quantidade": 1, "tamanho": 4},
+navios_info = {
+    "porta-avioes": {"quantidade": 1, "tamanho": 4},
     "navio-tanque": {"quantidade": 2, "tamanho": 3},
     "contratorpedeiro": {"quantidade": 3, "tamanho": 2},
-    "submarino": {"quantidade": 4, "tamanho": 1}}
+    "submarino": {"quantidade": 4, "tamanho": 1}
+}
 
 for nome_navio, info in navios_info.items():
     for i in range(info["quantidade"]):
         valido = False
         while not valido:
-            print(f"Insira as informações referentes ao navio {nome_navio} que possui tamanho {info['tamanho']}")
+            print(f"Insira as informacoes referentes ao navio {nome_navio} que possui tamanho {info['tamanho']}")
             linha = int(input("Linha: "))
             coluna = int(input("Coluna: "))
 
@@ -21,10 +28,11 @@ for nome_navio, info in navios_info.items():
                 orientacao = "vertical" if orient_input == 1 else "horizontal"
             else:
                 orientacao = "horizontal"
+
             if posicao_valida(frota, linha, coluna, orientacao, info["tamanho"]):
                 preenche_frota(frota, nome_navio, linha, coluna, orientacao, info["tamanho"])
                 valido = True
             else:
-                print("Esta posição não está válida!")
+                print("Esta posicao nao esta valida!")
 
 print(frota)
